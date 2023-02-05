f558bec0446fb770f043870b8f5c1008cb4c4310
Nacereddine Rebouh <57677323+NacereddineRebouh@users.noreply.github.com>
2023-02-05 20:40:27 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,28 +2,24 @@ export default function Head() {
   return (
     <>
       <meta
-        name="image"
         property="og:image"
-        content="https://the-state.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F2.3e438664.png&w=2048&q=100"
-     //  content="https://gustoso.vercel.app/og/ogimage.jpg"
+        content="https://gustoso.vercel.app/og/ogimage.jpg"
           />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/logo/logo2.png" />
 
       <title key="title">Gustoso: Pastry with Love</title>
       <meta
-        name="url"
         property="og:url"
         content="https://gustoso.vercel.app/"
       />
-      <meta name="type" property="og:type" content="website" />
+      <meta property="og:type" content="website" />
       <meta
-        name="title"
         property="og:title"
         content="Gustoso: Pastry with Love"
       />
       <meta
-        name="description"
+  
         property="og:description"
         content="Indulge in the tastiest flavors with Gustoso! Our daily fresh ingredients are combined to create irresistible dishes that will tantalize your taste buds. Get ready for a flavor explosion!"
       ></meta>
