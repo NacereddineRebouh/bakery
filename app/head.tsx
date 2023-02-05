e4d4feb974eb599422bf29399035fa772d1c80c6
Nacereddine_rbh <llbl2153@gmail.com>
2023-02-05 09:40:51 +0100

    fix: og image

--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,11 @@
 export default function Head() {
   return (
     <>
+      <meta
+        name="image"
+        property="og:image"
+        content="https://gustoso.vercel.app/og/ogimage.jpg"
+      />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/logo/logo2.png" />
 
@@ -21,11 +26,6 @@ export default function Head() {
         property="og:description"
         content="Indulge in the tastiest flavors with Gustoso! Our daily fresh ingredients are combined to create irresistible dishes that will tantalize your taste buds. Get ready for a flavor explosion!"
       ></meta>
-      <meta
-        name="image"
-        property="og:image"
-        content="https://cellafarm.vercel.app/og/ogimage.jpg"
-      />
     </>
   );
 }
