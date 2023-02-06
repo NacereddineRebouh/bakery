4d2e31a28cfbcaac34847fcd41f49e02dffe81fe
Nacereddine_rbh <llbl2153@gmail.com>
2023-02-06 08:49:03 +0100

    fix: og

--- app/head.tsx
+++ app/head.tsx
@@ -31,6 +31,7 @@ export default function Head() {
       />
       <meta
         name="description"
+        property="og:description"
         content="Indulge in the tastiest flavors with Gustoso! Our daily fresh ingredients are combined to create irresistible dishes that will tantalize your taste buds. Get ready for a flavor explosion!"
       ></meta>
     </>
